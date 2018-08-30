e654e3525b4931dd72db989c19b035ee39e566fa
Alejandro Bravo Torres <bravotorres.alejandro@gmail.com>
2018-08-30 01:19:33 -0500

    Base de Datos.

--- index.php
+++ index.php
@@ -1,20 +1,44 @@
 <html>
-	<head>
-		<title>Core Solutions</title>
-	</head>
-	<body>
-		<style>
-		body{
-			background-image: url("fondo.jpg");
-			background-size: 1400px 700px;
+<head>
+    <meta charset="UTF-8">
+    <title>Core Solutions</title>
+    <script src="static/js/jquery.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
+    <script src="static/css/bootstrap.min.css"></script>
+    <style>
+        body{
+            background-image: url("fondo.jpg");
+            background-size: 1400px 700px;
 
 
-		}
-		</style>
-		<ul>
-			<br><br><br><br><br><br><br><br><br><br><br><br><li><center><h1><a href="InsertarEspectacular.php"><font color="white">Insertar Espectacular</font></a></h1></center></li></br>
-			<br><li><center><h1><a href="EliminarEspectacular.php"><font color="white">Eliminar Espectacular</font></h1></center></li></br></br>
-			
-		</ul>
-	</body>
+        }
+    </style>
+</head>
+<body>
+
+<ul>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <li>
+        <center>
+            <h1>
+                <a href="InsertarEspectacular.php">
+                    <font color="white">Insertar Espectacular</font>
+                </a>
+            </h1>
+        </center>
+    </li>
+    <br>
+    <br>
+    <li>
+        <center>
+            <h1>
+                <a href="EliminarEspectacular.php">
+                    <font color="white">Eliminar Espectacular</font>
+            </h1>
+        </center>
+    </li>
+    <br>
+    <br>
+</ul>
+</body>
 </html>
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexx
+ * Date: 8/29/18
+ * Time: 4:13 PM
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio de Sesion</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <meta name=description content="">
+    <link rel="stylesheet" type="text/css" href="static/css/login.css">
+</head>
+<body>
+    <form action="">
+        <h2>Inicio de Sesion</h2>
+        <input type="text" placeholder="&#12272; Usuario" name="user">
+        <input type="text" placeholder="&#12272; Contrasena" name="pass">
+        <input type="submit" value="Ingresar">
+    </form>
+</body>
+</html>
